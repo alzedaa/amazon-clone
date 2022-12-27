be2f8ecba24197c6af5a37b4ec1e4b61eb8bc00d
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2022-12-27 18:30:49 -0500

    Starting the amazon project with a header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkUh6RBtNqgARkjm2xYEsj10RrzocsONo",
+  authDomain: "clone-87e3c.firebaseapp.com",
+  projectId: "clone-87e3c",
+  storageBucket: "clone-87e3c.appspot.com",
+  messagingSenderId: "6731204823",
+  appId: "1:6731204823:web:813cc064d309f3c25e8892",
+  measurementId: "G-P0HSJ4RBEW",
+};
