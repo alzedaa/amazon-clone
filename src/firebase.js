338711c19b279d8396f3591034fe7cf639e4c541
Ayman Alzedawy <ayman.alzedawy@gmail.com>
2023-01-03 13:37:27 -0500

    Implementing firebase into the application

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCkUh6RBtNqgARkjm2xYEsj10RrzocsONo",
   authDomain: "clone-87e3c.firebaseapp.com",
@@ -8,3 +10,5 @@ const firebaseConfig = {
   appId: "1:6731204823:web:813cc064d309f3c25e8892",
   measurementId: "G-P0HSJ4RBEW",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
